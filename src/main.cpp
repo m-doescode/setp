f533e3ec37ba1650d5f90fc01721ac997512f0f9
Majd Roaydi <majdroaydi@hotmail.com>
2022-10-07 17:03:09 +0300

    main.cpp | string -> wstring conversion

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <locale>
+#include <codecvt>
 #include "CLI/CLI.hpp"
 
 #include "path_operations.h"
@@ -14,8 +16,10 @@ using namespace setp::path_operations;
 
 CLI::App app;
 
+std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
 // CLI arguments
-wstring o_path;
+string o_path;
 path_location_t f_location = path_location_t::COMPUTER;
 path_position_t f_position = path_position_t::LAST;
 bool f_remove = false;
@@ -24,7 +28,8 @@ bool f_selfinstall = false;
 int main(int argc, char **argv)
 {
     app.description("Add a path to the PATH environment variable. Requires administrator privileges. If you do not have administrator access, you may use the -u flag to add it to the user environment instead.");
-    app.add_option("path", o_path, "The path to be added to the PATH environment variable. Use quotation marks for paths with spaces")->required()->type_name("PATH");
+    app.add_option("path", o_path, "The path to be added to the PATH environment variable. Use quotation marks for paths with spaces")
+        ->type_name("PATH");
     app.add_flag("-u,--user", f_location, "Adds the path to the user only, instead of on the Local PC");
     app.add_flag("-f,--first", f_position, "Adds the path to the beginning of the PATH");
     app.add_flag("-r,--remove", f_remove, "Removes the specified path from the PATH");
@@ -32,12 +37,20 @@ int main(int argc, char **argv)
 
     CLI11_PARSE(app, argc, argv);
 
+    if (!f_selfinstall && o_path.empty())
+    {
+        cout << "path is a required argument." << endl;
+        return -1;
+    }
+
+    wstring wpath = converter.from_bytes(o_path);
+
     if (f_selfinstall)
         setp::path_operations::install_self();
     else if (f_remove)
-        setp::path_operations::remove_path(o_path, f_location);
+        setp::path_operations::remove_path(wpath, f_location);
     else
-        setp::path_operations::add_path(o_path, f_location, f_position);
+        setp::path_operations::add_path(wpath, f_location, f_position);
 
 
     return 0;
