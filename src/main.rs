bf073e2499226c27edc80bec4d355c64e6cd0380
m-doescode <80221594+m-doescode@users.noreply.github.com>
2024-07-01 17:46:03 +0200

    Made messages more concise

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ mod commands;
 
 #[derive(Parser, Debug)]
 #[command(version, about = "The setp utility allows you to add or remove paths from the global PATH environment variable on \
-a Windows system. Paths can be added to the end or to the front and can be added either to the system environment variable with the -s \
-switch (requiring Administrator privileges) or locally to the user PATH variable instead (default). Local directories can also be added \
-with relative mode (-r). You may add the current directory using `setp add .` or the parent directory using `setp add ..` or a local directory in the same \
-path using `setp add ./<dir name>.`", long_about = None)]
+a Windows system. Use `setp add` followed by a relative or absolute path to append it to the path. If you want to remove a path, \
+use `setp remove` instead. To list all installed paths use `setp list` and to add a path to all users use `-s` (Note: Requires admin).", long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -16,10 +14,7 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    #[command(about="Adds paths to the environment variable.", after_help="By default, paths are added in raw mode and to the local user's environment variable. \
-    This means that paths are not checked to be valid and will be added as-is. For a safer evaluation, you may use the -r switch which will automatically resolve \
-    paths based on the current directory or from an absolute path. To modify the system environment variables, use the -s switch (An error will be thrown if the \
-    program is run without Administrator privileges). You may use the -p switch to add paths to the beginning of the variable instead of at the end.")]
+    #[command(about="Adds paths to the environment variable.")]
     Add{
         #[arg(long="prepend", short='p', help="Prepends paths to the beginning of the variable instead of at the end.")]
         prepend: bool,
@@ -28,11 +23,10 @@ enum Commands {
         args: ModifyArgs,
     },
 
-    #[command(about="Removes paths from the environment variable.", after_help="By default, paths are removed from the local user's environment variable. \
-    Use the -s switch to modify system environment variables. (An error will be thrown if the program is run without Administrator privileges).")]
+    #[command(about="Removes paths from the environment variable.")]
     Remove(ModifyArgs),
 
-    #[command(about="Lists paths from the environment variable", after_help="By default, paths are read from both the system and local user's environment \
+    #[command(about="Lists paths in the environment variable", after_help="By default, paths are read from both the system and local user's environment \
     variable and are merged together, system coming first, mimicking normal path resolution behavior. If you would like to only list system or user paths, \
     use the -s switch or -l switch for system or local user paths respectively. A message will be printed with this information for clarity's sake, so if \
     you want to process this information, use the -q switch to print it in raw mode to be processed by another program.")]
